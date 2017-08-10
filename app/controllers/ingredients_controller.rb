33dcfc0b60ed7f953a89e88278d0c9a3368f9f41
monica lai <monju4177@gmail.com>
2017-08-10 13:30:01 +0800

    ingredients

--- app/controllers/ingredients_controller.rb
+++ app/controllers/ingredients_controller.rb
@@ -1,9 +1,16 @@
 class IngredientsController < ApplicationController
+  before_action :set_ingredient, only: [:show]
+
   def index
     @ingredients = Ingredient.all
   end
 
   def show
+  end
+
+  private
+
+  def set_ingredient
     @ingredient = Ingredient.find(params[:id])
   end
 end
