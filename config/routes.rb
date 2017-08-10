ea067bcf9cfecab8ae7860a2ef7d3eca91ae5131
Zoey Tang <zt72@cornell.edu>
2017-08-10 13:27:08 +0800

    route chagnes

--- config/routes.rb
+++ config/routes.rb
@@ -1,27 +1,6 @@
 Rails.application.routes.draw do
-<<<<<<< HEAD
-  get 'ingredients/index'
 
-  get 'ingredients/show'
-
-  get 'doses/index'
-
-  get 'doses/new'
-
-  get 'doses/show'
-
-  get 'doses/create'
-
-  get 'doses/update'
-
-  get 'doses/edit'
-
-  get 'doses/destroy'
-
-=======
   resources :cocktails do
     resources :doses
   end
->>>>>>> f5d35878c225fcb0d0abc4caa666ff4ba5635dc3
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
