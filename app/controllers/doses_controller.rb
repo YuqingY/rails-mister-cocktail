08498619d117195a1d1039767977cb7570f7b964
Yuqing Yang <rachely.0208@gmail.com>
2017-08-09 22:40:25 -0700

    dose controller

--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -8,21 +8,43 @@ class DosesController < ApplicationController
   end
 
   def show
-    @dose = Dse.find(params[:id])
+    @dose = Dose.find(params[:id])
   end
 
   def create
     @dose = Dose.new(dose_params)
+    @dose.cocktail = Cocktail.find(params[:cocktail_id])
     if @dose.save
-      redirect
+      redirect_to cocktail_path(@dose.cocktail), notice: 'Dose was successfully created.'
+    else
+      render :new
+    end
   end
 
   def update
+    @dose.cocktail = Cocktail.find(params[:cocktail_id])
+    if @dose.update(dose_params)
+      redirect_to cocktail_path(@dose.cocktail), notice: 'Dose was successfully updated.'
+    else
+      render :new
+    end
   end
 
   def edit
+    @dose = Dose.find(params[:id])
   end
 
   def destroy
+    @cocktail = @dose.cocktail
+    @dose.destroy
+    redirect_to cocktail_path(@cocktail), notice: 'Dose was successfully deleted.'
+  end
+
+
+  private
+
+
+  def dose_params
+    params.require(:doses).permit(:amount_needed, :cocktail_id, :ingredient_id)
   end
 end
