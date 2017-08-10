f5d35878c225fcb0d0abc4caa666ff4ba5635dc3
Yuqing Yang <rachely.0208@gmail.com>
2017-08-09 21:58:52 -0700

    :routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,6 @@
 Rails.application.routes.draw do
-  get 'doses/index'
-
-  get 'doses/new'
-
-  get 'doses/show'
-
-  get 'doses/create'
-
-  get 'doses/update'
-
-  get 'doses/edit'
-
-  get 'doses/destroy'
-
+  resources :cocktails do
+    resources :doses
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
