dc22a88c737ad2bb4566deacf60c00f5419acc6b
Yuqing Yang <rachely.0208@gmail.com>
2017-08-10 00:12:36 -0700

    routes for rake

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,5 @@ Rails.application.routes.draw do
     resources :doses
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  delete 'doses/:id', to:"doses#destroy"
 end
